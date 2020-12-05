ea0f9c10517183abc80e04e2cf9a73b567cc7c47
Kiëd Llaentenn <kiedtl@tilde.team>
2020-12-05 19:07:30 +0000

    tui: add

---
+++ rt/tui.lua
@@ -0,0 +1,28 @@
+local tty = require('tty')
+local M = {}
+
+M.tty_height = 80
+M.tty_width  = 24
+
+function M.clean()
+	tty.line_wrap()
+	tty.clear()
+	tty.reset_scroll_area()
+	tty.main_buffer()
+	io.flush(1)
+end
+
+function M.refresh()
+	M.tty_height, M.tty_width = tty.dimensions()
+
+	assert(M.tty_height)
+	assert(M.tty_width)
+
+	tty.alt_buffer()
+	tty.no_line_wrap()
+	tty.clear()
+	tty.set_scroll_area(M.tty_height - 1)
+	tty.curs_move_to_line(999)
+end
+
+return M
