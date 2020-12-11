56bd0ed0ca07a30d3b060a60dd39ddae293b4525
Kiëd Llaentenn <kiedtl@tilde.team>
2020-12-11 01:01:36 +0000

    tbrl: proper backspacing

--- rt/tbrl.lua
+++ rt/tbrl.lua
@@ -25,7 +25,7 @@ function M.on_key_event(event, enter_callback, keyseq_callback)
 		if event.key == tb.TB_KEY_BACKSPACE
 				or event.key == tb.TB_KEY_BACKSPACE2 then
 			if M.cursor == 0 then return end
-			M.bufin = M.bufin:sub(1, M.cursor+1) .. M.bufin:sub(M.cursor+3, #M.bufin)
+			M.bufin = M.bufin:sub(1, M.cursor-1) .. M.bufin:sub(M.cursor+2, #M.bufin)
 			M.cursor = M.cursor - 1
 		elseif event.key == tb.TB_KEY_SPACE then
 			M.bufin = M.bufin:gsub(("."):rep(M.cursor), "%1 ")
