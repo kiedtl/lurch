95b18d48ab0f1a119b0590b3fe655c10ad2d25fa
Kiëd Llaentenn <kiedtl@tilde.team>
2020-12-13 22:28:49 +0000

    add more irc tests

--- test/irc_test.lua
+++ test/irc_test.lua
@@ -1,5 +1,6 @@
 local lunatest = package.loaded.lunatest
 local assert_true = lunatest.assert_true
+local assert_false = lunatest.assert_false
 local assert_equal = lunatest.assert_equal
 
 local irc = require("irc")
@@ -33,6 +34,20 @@ function M.test_multi_tags_mixed_value()
     assert_true(e.tags.tag3); assert_true(e.tags.tag4)
     assert_equal(e.tags.tag1, "");     assert_equal(e.tags.tag2, "23")
     assert_equal(e.tags.tag3, "dsaf"); assert_equal(e.tags.tag4, "")
+end
+
+function M.test_sender_parsing()
+    local e1 = irc.parse(":yellor73!gabtish@host.com PRIVMSG #atem :CKUUUUUF")
+    assert_equal(e1.from, "yellor73!gabtish@host.com")
+    assert_equal(e1.nick, "yellor73")
+    assert_equal(e1.user, "gabtish")
+    assert_equal(e1.host, "host.com")
+
+    local e2 = irc.parse(":meat.edlit.chat NOTE * :*** Gnikool pu-rouy stoheman...")
+    assert_equal(e2.from, "meat.edlit.chat")
+    assert_equal(e2.from, e2.host)
+    assert_false(e2.user)
+    assert_false(e2.nick)
 end
 
 return M
